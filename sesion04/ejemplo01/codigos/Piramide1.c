d3efeea6ef081586727043446d3ca8dd5bff889d
manu-msr <manu@ciencias.unam.mx>
2020-03-04 11:33:08 -0600

    S4 :: Códigos

---
+++ sesion04/ejemplo01/codigos/Piramide1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Programa que imprime una pirámide de cinco niveles. */
+
+int main(void)
+{
+   // Impresión de cadenas.
+   printf("    *    \n");
+   printf("   ***   \n");
+   printf("  *****  \n");
+   printf(" ******* \n");
+   printf("*********\n");
+   
+   system("pause"); // para DevC++
+   return 0;
+}
---
+++ sesion04/ejemplo01/codigos/Piramide2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Programa que imprime una pirámide de cinco niveles. */
+
+int main(void)
+{
+   // Impresión de cadenas.
+   printf("    *    \n   ***   \n  *****  \n ******* \n*********\n");
+   
+   //system("pause"); // para DevC++
+   return 0;
+}
